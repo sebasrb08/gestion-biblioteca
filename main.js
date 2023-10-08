dae275a1b4fcc4227acd88c93acc4fc5cf0423eb
sebastian <sebasr1268@gmail.com>
2023-10-07 20:25:53 -0500

    boton eliminar implementado

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
+//Obtengo las etiquetas desde mi HTML
 let enviar =document.querySelector("#enviar__info")
 let informacion=document.querySelectorAll(".info")
 let tarjetas=document.querySelector(".tarjetas")
 let modal = document.querySelector(".modales")
 let boton = document.querySelector(".boton")
 let ordenar = document.querySelector(".ordenar")
+let nombre = document.querySelector("#nombre")
+let cambios = document.querySelector("#cambios")
+let elimi
+
 
 let cont =1
 let cambiar
+let index
+let prestado
+let botonesestado
+// Obtengo los el objeto biblioteca
 let biblioteca = JSON.parse(localStorage.getItem("biblioteca"))
 
-mostrarTarjeta()
+function permanenciaDatos(){
+    localStorage.setItem("biblioteca",JSON.stringify(biblioteca))
+
+}
 
 function mostrarTarjeta(){
     if(biblioteca){
@@ -38,48 +50,207 @@ function mostrarTarjeta(){
             año.textContent=`Año:${element.año}`
 
             let estado = document.createElement("p")
+            estado.classList.add("estado")
             estado.textContent=`Estado:${element.estado}`
 
-            let botonEstado = document.createElement("button");
-            botonEstado.classList.add("btn","btn-success")
-            botonEstado.textContent="Cambiar Estado"
-            // let prestar = document.createElement("p")
-            // prestar.textContent=`Prestado A:${element.prestar}`
+            prestado = document.createElement("p")
+            prestado.classList.add("prestados")
+            prestado.textContent=`Prestado A:${element.prestar}`
 
+            if(element.prestar == null){
+                prestado.style.display="none"
+            }else{
+                prestado.style.display="block"
+
+            }
+
+            let botonEstado = document.createElement("button");
+            botonEstado.classList.add("btn","btn-success","w-100")
+            botonEstado.textContent="Cambiar Estado"
+
+            if(element.estado == "Disponible"){
+                botonEstado.setAttribute("data-bs-toggle","modal")
+                botonEstado.setAttribute("data-bs-target","#exampleModal")
+            }else{
+                botonEstado.removeAttribute("data-bs-toggle")
+                botonEstado.removeAttribute("data-bs-target")
+            }
+
+            let botoneliminar = document.createElement("button");
+            botoneliminar.classList.add("btn","btn-danger","w-100","mt-2")
+            botoneliminar.textContent="Eliminar"
             
             div2.appendChild(h)
             div2.appendChild(autores)
             div2.appendChild(año)
             div2.appendChild(estado)
+            div2.appendChild(prestado)
             div2.appendChild(botonEstado)
-
-            // div2.appendChild(prestar)
-
+            div2.appendChild(botoneliminar)
 
             
             div.appendChild(img)
             div.appendChild(div2)
             
             tarjetas.appendChild(div)
+            
+            
+        });
+        
+    }
+    elimi= document.querySelectorAll(".btn-danger")
+    borrarTarjeta()
+    cambiar=document.querySelectorAll(".btn-success")
+    cambiarEstado(cambiar)
+
+}
+
+function añadirTarjeta(){
+    let ele=crearElementos()
+
+    for (i=2;i<ele.length;i++){
+
+        ele[1].appendChild(ele[i])
+    }
+            
+    ele[0].appendChild(ele[2])
+    ele[0].appendChild(ele[1])
+            
+    tarjetas.appendChild(ele[0])
 
           
-        });
-        cambiar=document.querySelectorAll(".btn-success")
+    cambiar=document.querySelectorAll(".btn-success")
+    let pres=ele[7]
+    let boton=ele[8]
+    cambiarEstado(cambiar,pres,boton)
+    
 }
-    for (let i = 0; i < cambiar.length; i++) {
-        cambiar[i].addEventListener("click",()=>{
-            if (biblioteca[i].estado == "Disponible"){
-                
-                biblioteca[i].estado="Prestado"
-                
+
+function crearElementos (){
+    let div = document.createElement("div")
+    div.classList.add("card")
+
+            
+    let img = document.createElement("img")
+    img.classList.add("card-img-top")
+    img.src=biblioteca[biblioteca.length-1].url
+            
+    let div2 = document.createElement("div")
+    div2.classList.add("card-body")
+            
+    let h = document.createElement("h5")
+    h.classList.add("card-title")
+    h.textContent=biblioteca[biblioteca.length-1].titulo
+
+    let autores = document.createElement("p")
+    autores.textContent=`Autor:${biblioteca[biblioteca.length-1].autor}`
+            
+
+    let año = document.createElement("p")
+    año.textContent=`Año:${biblioteca[biblioteca.length-1].año}`
+
+    let estado = document.createElement("p")
+    estado.classList.add("estado")
+    estado.textContent=`Estado:${biblioteca[biblioteca.length-1].estado}`
+
+    let prestado = document.createElement("p")
+    prestado.classList.add("prestados")
+    prestado.textContent=`Prestado A:${biblioteca[biblioteca.length-1].prestar}`
+
+    if(biblioteca[biblioteca.length-1].prestar == null){
+            prestado.style.display="none"
+        }else{
+            prestado.style.display="block"            
+        }
+
+    let botonEstado = document.createElement("button");
+    botonEstado.classList.add("btn","btn-success","w-100")
+    botonEstado.textContent="Cambiar Estado"
+            
+    if(biblioteca[biblioteca.length-1].estado == "Disponible"){
+        botonEstado.setAttribute("data-bs-toggle","modal")
+        botonEstado.setAttribute("data-bs-target","#exampleModal")
+    }else{
+        botonEstado.removeAttribute("data-bs-toggle")
+        botonEstado.removeAttribute("data-bs-target")
+    
+    }
+    let botoneliminar = document.createElement("button");
+    botoneliminar.classList.add("btn","btn-danger","w-100","mt-2")
+    botoneliminar.textContent="Eliminar"
+     
+    let elementos=[div,div2,img,h,autores,año,estado,prestado,botonEstado,botoneliminar]
+    return elementos
+}
+
+function cambiarEstado(cambiar){
+    if(biblioteca){
+        for (let i = 0; i < cambiar.length; i++) {
+            cambiar[i].addEventListener("click",()=>{
+                index=i
+                if(biblioteca[i].estado == "Prestado"){
+                    biblioteca[index].estado="Disponible"
+                    biblioteca[index].prestar=null
+
+                    cambiarEstadoParrafo(index)
+                    mostrarPrestado(index)
+                    mostrarModal(index)
+                    permanenciaDatos()
+
+                }
+        
+            })
+        }
+    }
+    cambios.addEventListener("click",()=>{
+        if (biblioteca[index].estado == "Disponible"){
+                    
+            biblioteca[index].estado="Prestado"
+            biblioteca[index].prestar=nombre.value
+
+            cambiarEstadoParrafo(index)
+            mostrarPrestado(index)
+            mostrarModal(index)
+            permanenciaDatos()
+            
+            reset2()
+        }
+        
+    })
+}
+
+function cambiarEstadoParrafo(i){
+    let estados= document.querySelectorAll(".estado")
+    estados[i].textContent=`Estado:${biblioteca[i].estado}`
+}
+
+
+
+function mostrarPrestado(index){
+    prestado=document.querySelectorAll(".prestados")
+            if(biblioteca[index].prestar == null){
+                prestado[index].style.display="none"
             }else{
-                biblioteca[i].estado="Disponible"
+                prestado[index].style.display="block"
+                prestado[index].textContent=`Prestado A: ${biblioteca[index].prestar}`
             }
-            borrar()
-            mostrarTarjeta()
-        })
-    } 
-}  
+    
+}
+
+function mostrarModal(index,botonEstado){
+    botonEstado=document.querySelectorAll(".estado")
+        if(biblioteca[index].estado == "Disponible"){
+            botonEstado[index].setAttribute("data-bs-toggle","modal")
+            botonEstado[index].setAttribute("data-bs-target","#exampleModal")
+            console.log("2") 
+        }else{
+             botonEstado[index].removeAttribute("data-bs-toggle")
+             botonEstado[index].removeAttribute("data-bs-target")
+             console.log("1")
+         }
+
+}
+
 function borrar(){
     div=document.querySelectorAll(".card")
     console.log(div)
@@ -96,6 +267,9 @@ function reset(){
     
 }
 
+function reset2(){
+    nombre.value=""
+}
 
 enviar.addEventListener("click",()=>{
     if(biblioteca){
@@ -110,9 +284,6 @@ enviar.addEventListener("click",()=>{
             url:informacion[3].value
             }
         )
-        console.log(biblioteca)
-        borrar()
-        
     }else{
         biblioteca=[{
             titulo:informacion[0].value,
@@ -124,14 +295,12 @@ enviar.addEventListener("click",()=>{
         }]
         console.log(biblioteca)
     }
-    localStorage.setItem("biblioteca",JSON.stringify(biblioteca))
+    permanenciaDatos()
 
-
-    mostrarTarjeta()
+    añadirTarjeta()
     reset()
 
 })
-
 
 boton.addEventListener("click",()=>{
     let valor=ordenar.value
@@ -163,11 +332,25 @@ boton.addEventListener("click",()=>{
         })
     }
 
-    console.log(biblioteca);
     borrar()
     mostrarTarjeta()
-
-
-
 })
+function borrarTarjeta(){
 
+    for (let i = 0; i < elimi.length; i++) {
+        elimi[i].addEventListener("click", ()=>{
+            
+            biblioteca.splice(i,1)
+            console.log(biblioteca)
+            permanenciaDatos()
+            borrar()
+            mostrarTarjeta()
+        })
+        
+    }
+}
+    
+
+
+
+mostrarTarjeta()
